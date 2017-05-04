6c488979f48648540ca14b0dd487b66a9d17dd13
Jeff Mixon <jeff.mixon@gmail.com>
2017-05-03 21:52:14 -0700

    consider no-undef an error; add check for guard-for-in as an error

--- test/index.js
+++ test/index.js
@@ -1,13 +1,19 @@
-var eslint = require('eslint')
-var test = require('tape')
-var path = require('path')
+var eslint = require('eslint');
+var test = require('tape');
+var path = require('path');
 
 var linter = new eslint.CLIEngine({
-  configFile: path.join(__dirname, '..', 'eslintrc.json')
-})
+	configFile: path.join(__dirname, '..', 'eslintrc.json')
+});
 
 test('api: lintText', function (t) {
-  t.plan(1)
-  var result = linter.executeOnText("console.log('hi there')\n\n")
-  t.equals(result.results[0].messages[0].message, 'Missing semicolon.')
-})
+	t.plan(1);
+	var result = linter.executeOnText("console.log('hi there')\n\n");
+	t.equals(result.results[0].messages[0].message, 'Missing semicolon.');
+});
+
+test('api: guard for-in', function (t) {
+	t.plan(1);
+	var result = linter.executeOnText("var arr = ['one', 'two', 'three']; for (var i in arr) { console.log(i); }");
+	t.equals(result.results[0].messages[0].message, 'The body of a for-in should be wrapped in an if statement to filter unwanted properties from the prototype.');
+});
